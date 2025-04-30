8837ace97de599cd00976968e894003093d7752c
Oualid Saa <81866278+ousax@users.noreply.github.com>
2025-04-30 19:58:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('infoButton');
-
-  button.addEventListener('click', () => {
-    alert('Hello from FCA Projet! You clicked the button.');
+    const form = document.getElementById('loginForm');
+    const resetLink = document.getElementById('resetPassword');
+    const message = document.getElementById('loginMessage');
+  
+    if (form) {
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+  
+        if (username === 'admin' && password === '1234') {
+          window.location.href = 'home.html';
+        } else {
+          message.textContent = 'Incorrect username or password!';
+          message.style.color = 'red';
+        }
+      });
+    }
+  
+    if (resetLink) {
+      resetLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        alert('A password reset link has been sent to your email!');
+      });
+    }
   });
-});
+  
