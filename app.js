82597a7879d3fb69c2317245f548cf477f4b2e34
Oualid Saa <81866278+ousax@users.noreply.github.com>
2025-04-30 17:19:59 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('infoButton');
+
+  button.addEventListener('click', () => {
+    alert('Hello from FCA Projet! You clicked the button.');
+  });
+});
